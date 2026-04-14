e8190023abd06bf4c6cf0c95dd69bd25ae7c0464
Orpheusolaf <114246933+Orpheusolaf@users.noreply.github.com>
2026-04-14 16:36:12 +0200

    AddressQuery.kt

---
+++ app/src/main/java/com/example/brusselscollectiondemo/data/AddressQuery.kt
@@ -0,0 +1,8 @@
+package com.example.brusselscollectiondemo.data
+
+data class AddressQuery(
+    val street: String,
+    val number: String,
+    val postalCode: String,
+    val municipality: String
+)
