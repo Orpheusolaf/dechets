86e0e1aa86b1c2eed159ec81663ce919db421c54
Orpheusolaf <114246933+Orpheusolaf@users.noreply.github.com>
2026-04-14 17:47:40 +0200

    MainActivity.kt

--- app/src/main/java/com/example/brusselscollectiondemo/MainActivity.kt
+++ app/src/main/java/com/example/brusselscollectiondemo/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -30,19 +31,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.brusselscollectiondemo.ui.CollectionViewModel
-import com.example.brusselscollectiondemo.ui.theme.BrusselsCollectionDemoTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BrusselsCollectionDemoTheme {
+            MaterialTheme {
                 CollectionApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CollectionApp(viewModel: CollectionViewModel = viewModel()) {
     val uiState by viewModel.uiState.collectAsState()
