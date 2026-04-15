37578db3ae21ec4a439b42b652cc03482e2c24c8
Orpheusolaf <114246933+Orpheusolaf@users.noreply.github.com>
2026-04-15 11:57:13 +0200

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
